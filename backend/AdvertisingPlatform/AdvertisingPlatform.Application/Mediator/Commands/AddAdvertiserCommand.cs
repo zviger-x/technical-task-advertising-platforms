8b311e55c5dc01ad3215f6218cfc34a0c985fb51
AlexeyMarkevich <zviger.alexey@gmail.com>
2025-07-29 13:53:54 +0300

    Added mediator commands and queries

---
+++ backend/AdvertisingPlatform/AdvertisingPlatform.Application/Mediator/Commands/AddAdvertiserCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace AdvertisingPlatform.Application.Mediator.Commands
+{
+    public sealed class AddAdvertisersCommand : IRequest
+    {
+        public required Stream FileStream { get; init; }
+    }
+}
